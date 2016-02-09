fea77e313a5a76bfc6ff73718db05343a3495858
lisroach <lisroach@cisco.com>
2016-02-09 21:21:12 +0000

    rewrote class so that you add data_source to every call instead of once in constructor.

--- restClass.py
+++ restClass.py
@@ -1,3 +1,5 @@
+"""This module contains a class for making RESTcalls with Python"""
+
 from requests.auth import HTTPBasicAuth
 import requests
 import re
@@ -5,60 +7,123 @@ import sys
 
 
 class restCalls(object):
-    def __init__(self, username, password, ip_address_port, data_source):
-        self.auth = HTTPBasicAuth(username, password)
-        self.data_source = data_source
-        #Check the data file type if a file has been provided
-        if isinstance(self.data_source, file):
-            self.data_type = self.check_data_type()
-            self.headers = ({
-                'Accept': 'application/yang.errors+{}'.format(
-                    self.data_type[0]),
-                'Content-Type': 'application/yang.data+{}'.format(
-                    self.data_type[0])
-                })
-            self.url = "http://{}/restconf/data/{}".format(
-                ip_address_port, self.data_type[1].group(1))
-        #If the second argument is a string it must be a GET or DELETE
-        elif isinstance(self.data_source, str):
-            self.url = "http://{}/restconf/data/{}".format(
-                ip_address_port, self.data_source)
-            self.headers = ({
-                'Accept':
-                'application/yang.data+json, application/yang.errors+json',
-                'Content-Type': 'application/yang.data+json'
-                })
+    """This class creates RESTconf calls using python.
 
-    def put(self):
-        response = requests.put(url=self.url, headers=self.headers,
-                                auth=self.auth, data=self.data_source)
+        :param username: Username for device login
+        :param password: Password for device login
+        :param ip_address_port: The ip address and port number for the device
+        :type password: str
+        :type username: str
+        :type ip_address_port: str
+    """
+    def __init__(self, username, password, ip_address_port):
+        self.auth = HTTPBasicAuth(username, password)
+        self.ip_address_port = ip_address_port
+
+    def put(self, data_file):
+        """PUT RESTconf call
+            :param data_file: JSON or XML file with config changes
+            :type data_file: File object
+            :return: Return the response object
+            :rtype: Response object
+        """
+        headers = self.create_headers(data_file)
+        response = requests.put(url=headers[0], headers=headers[1],
+                                auth=self.auth, data=data_file)
         return response
 
-    def post(self):
+    def post(self, data_file):
+        """POST RESTconf call
+            :param data_file: JSON or XML file with config changes
+            :type data_file: File object
+            :return: Return the response object
+            :rtype: Response object
+        """
         response = requests.post(url=self.url, headers=self.headers,
                                  auth=self.auth, data=self.data_source)
         return response
 
-    def patch(self):
+    def patch(self, data_file):
+        """PATCH RESTconf call
+            :param data_file: JSON or XML file with config changes
+            :type data_file: File object
+            :return: Return the response object
+            :rtype: Response object
+        """
         response = requests.patch(url=self.url, headers=self.headers,
                                   auth=self.auth, data=self.data_source)
         return response
 
-    def get(self):
-        url = self.url + "?content=config"
-        response = requests.get(url=url, headers=self.headers,
+    def get(self, choice):
+        """GET RESTconf call
+            :param data_file: String selection of YANG model and local YANG
+            :type data_file: str
+            :return: Return the response object
+            :rtype: Response object
+        """
+        headers = self.create_headers(choice)
+        url = headers[0] + "?content=config"
+        response = requests.get(url=url, headers=headers[1],
                                 auth=self.auth)
         return response
 
-    def check_data_type(self):
+    def delete(self, choice):
+        """GET RESTconf call
+            :param data_file: String selection of YANG model and local YANG
+            :type data_file: str
+            :return: Return the response object
+            :rtype: Response object
+        """
+        headers = self.create_headers(choice)
+        response = requests.delete(url=headers[0], headers=headers[1],
+                                   auth=self.auth)
+        return response
+
+    def create_headers(self, data_source):
+        """Generate the headers
+            :param data_source: The file or yang model name
+            :type data_source: Either a string or file object
+            :return: Return a tuple with the header information
+            :rtype: tuple
+
+        """
+        if isinstance(data_source, file):
+            data_type = self.check_data_type(data_source)
+            headers = ({
+                'Accept': 'application/yang.errors+{}'.format(
+                    data_type[0]),
+                'Content-Type': 'application/yang.data+{}'.format(
+                    data_type[0])
+                })
+            url = "http://{}/restconf/data/{}".format(
+                self.ip_address_port, data_type[1].group(1))
+        #If the second argument is a string it must be a GET or DELETE
+        elif isinstance(data_source, str):
+            url = "http://{}/restconf/data/{}".format(
+                self.ip_address_port, data_source)
+            headers = ({
+                'Accept':
+                'application/yang.data+json, application/yang.errors+json',
+                'Content-Type': 'application/yang.data+json'
+                })
+        else:
+            #If it isnt a file or a string something is wrong
+            url = "error"
+            headers = "error"
+        return (url, headers)
+
+    def check_data_type(self, data_source):
         """Check the data type of file and the YANG model
 
+            :param data_source: The file with JSON or XML
+            :type data_source: Open file object
             :return: Returns the type of data in the file and the
-                     yang model name
+                     yang model name and local name
             :rtype: tuple(string, string)
 
         """
-        line = [next(self.data_source) for x in xrange(2)]
+        #Grab the first two lines in the file
+        line = [next(data_source) for x in xrange(2)]
         first_string = line[0]
         if first_string[:1] == '{' or first_string[:1] == '[':
             data_type = 'json'
--- rest_test.py
+++ rest_test.py
@@ -1,3 +1,14 @@
+"""Tests the restClass module.
+
+    You should comment out all the tests for restCalls that you are not testing
+    at that moment, as they can interfere with each other. Sometimes status
+    messages may change, for example, both 200 and 204 are successful messages.
+    204 indicates that the call worked, there was just no content to be
+    returned.
+
+    Fill in your own username, password, ip address and port, and data source.
+"""
+
 import unittest
 from restClass import restCalls
 
@@ -6,89 +17,110 @@ class restCallsCase(unittest.TestCase):
     username = 'username'
     password = 'password'
     ip_address_port = 'ip_address:port'
-    data_source = 'bgp:bgp'
+    data_source = 'yang_module:container'
 
     def test_does_get_init_work(self):
-        """Does constructor create a proper object for GET and DELETE"""
+        """Does constructor create a proper object"""
         classObject = restCalls(self.username, self.password,
-                                self.ip_address_port, self.data_source)
-        self.assertEqual(classObject.url,
-                         'http://10.200.96.52:2580/restconf/data/bgp:bgp')
-        self.assertEqual(classObject.data_source, 'bgp:bgp')
+                                self.ip_address_port)
+        self.assertEqual(classObject.ip_address_port,
+                         'ip_address:port')
 
     def test_check_data_json(self):
         """Test the check_data_type method for json"""
-        with open('data.json', 'rb') as read_file:
-            classObject = restCalls(self.username, self.password,
-                                    self.ip_address_port, read_file)
-            self.assertIsInstance(classObject.data_type, tuple)
-            self.assertEqual(classObject.data_type[0], 'json')
-            self.assertEqual(classObject.data_type[1].group(1), 'bgp:bgp')
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        with open('data.json', 'rb') as data_file:
+            data_type = classObject.check_data_type(data_file)
+            self.assertIsInstance(data_type, tuple)
+            self.assertEqual(data_type[0], 'json')
+            self.assertEqual(data_type[1].group(1), 'yang_module:container')
 
     def test_check_data_xml(self):
         """Test the check_data_type method for xml"""
-        with open('data.xml', 'rb') as read_file:
-            classObject = restCalls(self.username, self.password,
-                                    self.ip_address_port, read_file)
-            self.assertIsInstance(classObject.data_type, tuple)
-            self.assertEqual(classObject.data_type[0], 'xml')
-            self.assertEqual(classObject.data_type[1].group(1), 'bgp:bgp')
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        with open('data.xml', 'rb') as data_file:
+            data_type = classObject.check_data_type(data_file)
+            self.assertIsInstance(data_type, tuple)
+            self.assertEqual(data_type[0], 'xml')
+            self.assertEqual(data_type[1].group(1), 'yang_module:container')
+
+    def test_header_creation(self):
+        """Test the create_header method"""
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        with open('data.json', 'rb') as data_file:
+            items = classObject.create_headers(data_file)
+            self.assertIsInstance(items, tuple)
+            self.assertEqual(len(items), 2)
+            self.assertEqual(items[0], 'http://{}/restconf/data/yang_module:container'.format(
+                             self.ip_address_port))
+            self.assertEqual(items[1], ({
+                'Accept':
+                'application/yang.errors+json',
+                'Content-Type': 'application/yang.data+json'
+                }))
 
     def test_check_bad_json(self):
         """Test the check_data_type method for bad json"""
-        with open('bad_data.json', 'rb') as read_file:
-            with self.assertRaises(SystemExit) as cm:
-                restCalls(self.username, self.password,
-                          self.ip_address_port, read_file)
-            self.assertEqual(cm.exception.code,
-                             "Your data file has malformed XML or JSON.")
+        with self.assertRaises(SystemExit) as cm:
+            classObject = restCalls(self.username, self.password,
+                                    self.ip_address_port)
+            with open('bad_data.json', 'rb') as data_file:
+                classObject.check_data_type(data_file)
+        self.assertEqual(cm.exception.code,
+                         "Your data file has malformed XML or JSON.")
 
     def test_check_bad_xml(self):
         """Test the check_data_type method for bad xml"""
-        with open('bad_data.xml', 'rb') as read_file:
-            with self.assertRaises(SystemExit) as cm:
-                restCalls(self.username, self.password,
-                          self.ip_address_port, read_file)
+        with self.assertRaises(SystemExit) as cm:
+            classObject = restCalls(self.username, self.password,
+                                    self.ip_address_port)
+            with open('bad_data.xml', 'rb') as data_file:
+                classObject.check_data_type(data_file)
             self.assertEqual(cm.exception.code,
                              "Your data file has malformed XML or JSON.")
 
     def test_get_request(self):
         """Test if GET returns a proper object"""
         classObject = restCalls(self.username, self.password,
-                                self.ip_address_port, self.data_source)
-        item = classObject.get()
+                                self.ip_address_port)
+        item = classObject.get('yang_module:container')
         self.assertIsInstance(item.content, str)
+        #204 or 200 statuses are good
+        self.assertEqual(item.status_code, 204)
 
-#    def test_put_request(self):
-#        """Test if PUT adds a configuration"""
-#        with open('data.json', 'rb') as read_file:
-#            classObject = restCalls(self.username, self.password,
-#                                    self.ip_address_port, read_file)
-#            item = classObject.put()
-#            self.assertEqual(item.status_code, 204)
+    def test_put_request(self):
+        """Test if PUT adds a configuration"""
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        with open('data.json', 'rb') as data_file:
+            item = classObject.put(data_file)
+        self.assertEqual(item.status_code, 204)
 
     def test_post_request(self):
         """Test if POST adds a configuration"""
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
         with open('data.json', 'rb') as read_file:
-            classObject = restCalls(self.username, self.password,
-                                    self.ip_address_port, read_file)
-            item = classObject.post()
-            self.assertEqual(item.status_code, 201)
+            item = classObject.post(read_file)
+        self.assertEqual(item.status_code, 201)
 
-#    def test_patch_request(self):
-#        """Test if PATCH adds a configuration"""
-#        with open('data.json', 'rb') as read_file:
-#            classObject = restCalls(self.username, self.password,
-#                                    self.ip_address_port, read_file)
-#            item = classObject.patch()
-#            self.assertEqual(item.status_code, 204)
+    def test_patch_request(self):
+        """Test if PATCH adds a configuration"""
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        with open('data.json', 'rb') as read_file:
+            item = classObject.patch(read_file)
+        self.assertEqual(item.status_code, 204)
 
-#    def test_delete_request(self):
-#        """Test if DELETE removes the configuration"""
-#        classObject = restCalls(self.username, self.password,
-#                                self.ip_address_port, self.data_source)
-#        item = classObject.delete()
-#        self.assertEqual(item.status_code, 204)
+    def test_delete_request(self):
+        """Test if DELETE removes the configuration"""
+        classObject = restCalls(self.username, self.password,
+                                self.ip_address_port)
+        item = classObject.delete('yang_mode:container')
+        self.assertEqual(item.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
