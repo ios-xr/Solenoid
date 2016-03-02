739551b11c326515b03e59110e5e940234fa9631
kkumara3 <kkumara3@cisco.com>
2016-03-02 13:09:33 -0800

    Clean Up

--- static/refresh.js
+++ static/refresh.js
@@ -15,15 +15,21 @@
             var cell1 = row.cells[0];
             var cell2 = row.cells[1];
             cell1.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['prefix'];
-            cell2.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['vrf-route']['segment-route-next-hop-table']['vrf-next-hop-next-hop-address'][0]['next-hop-address']; 
+            cell2.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['vrf-route']['segment-route-next-hop-table']['vrf-next-hop-next-hop-address'][0]['next-hop-address'];
           } else {
             var table = document.getElementById("rib");
             var row = table.insertRow(i+1);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             cell1.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['prefix'];
-            cell2.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['vrf-route']['segment-route-next-hop-table']['vrf-next-hop-next-hop-address'][0]['next-hop-address']; 
+            cell2.innerHTML = json['Cisco-IOS-XR-ip-static-cfg:vrf-prefixes']['vrf-prefix'][i]['vrf-route']['segment-route-next-hop-table']['vrf-next-hop-next-hop-address'][0]['next-hop-address'];
           }
+        }
+        if (keys<rows) {
+          do {
+            var table = document.getElementById("rib");
+            table.deleteRow(keys + 1);
+          } while (keys < rows);
         }
       }
     };
@@ -31,20 +37,29 @@
     xhttp.send();
   }
   function getInfo1(){
+    var Table = document.getElementById("exa");
+    var rows_l = Table.rows.length
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
         var exa = JSON.parse(xhttp.responseText);
-        document.getElementById("Peer_IP").innerHTML = exa['neighbor']['ip'];
-        var counter = Object.keys(exa['neighbor']['message']['update']).length - 1
-        var update = Object.keys(exa['neighbor']['message']['update'])[counter];
-        document.getElementById("Update_Type").innerHTML = update;
-        var nexthop = Object.keys(exa['neighbor']['message']['update'][update]['ipv4 unicast']);
-        document.getElementById("Next_Hop").innerHTML = nexthop;
-        var network = Object.keys(exa['neighbor']['message']['update'][update]['ipv4 unicast'][nexthop]);
-        document.getElementById("Update_Network").innerHTML = network;
-        var time = timeConverter(exa['time'])
-        document.getElementById("Update_Time").innerHTML = time;
+        var time = timeConverter(exa['time']);
+        var old_time = Table.rows[rows_l+1].cells[4].innerHTML;
+        if (time != old_time){
+          var row = Table.insertRow(rows_l)
+          row.insertCell(0).innerHTML = exa['neighbor']['ip'];
+          var counter = Object.keys(exa['neighbor']['message']['update']).length - 1
+          var update = Object.keys(exa['neighbor']['message']['update'])[counter];
+          row.insertCell(1).innerHTML = update;
+          var nexthop = Object.keys(exa['neighbor']['message']['update'][update]['ipv4 unicast']);
+          row.insertCell(2).innerHTML = nexthop;
+          var network = Object.keys(exa['neighbor']['message']['update'][update]['ipv4 unicast'][nexthop]);
+          row.insertCell(3).innerHTML = network;
+          row.insertCell(4).innerHTML = time;
+        }
+        if (rows_l > 6){
+          Table.deleteRow(1)
+        }
       }
     };
     xhttp.open('GET', '/get_exa_json', true);
