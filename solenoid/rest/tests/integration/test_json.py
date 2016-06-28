0bd7e970e5f2a55e367e060c6f6731f99b8ddb37
lisa roach <lisroach@cisco.com>
2016-06-28 22:13:02 +0000

    Updating unit test with config file.

--- solenoid/rest/tests/integration/test_json.py
+++ solenoid/rest/tests/integration/test_json.py
@@ -1,18 +1,17 @@
 """These unit tests test the functionality of each function, not the full program."""
 import unittest
 import os
-import mock
-
+import ConfigParser
 from rest_calls.jsonRestClient import JSONRestCalls
 
 here = os.path.dirname(os.path.realpath(__file__))
-
-
-username = 'admin'
-password = 'admin'
-ip = '127.0.0.1'
-port = 80
-endpoint = 'Cisco-IOS-XR-ip-static-cfg:router-static'
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(here, 'restcalls.config'))
+ip = config.get('default', 'ip')
+port = int(config.get('default', 'port'))
+username = config.get('default', 'username')
+password = config.get('default', 'password')
+endpoint = 'Cisco-IOS-XR-ipv4-ospf-cfg:ospf'
 sm_url = '{scheme}://{ip}:{port}{basePath}/'.format(
     scheme='http',
     ip=ip,
@@ -46,76 +45,52 @@ class JSONRestCallsCase(unittest.TestCase):
                          headers)
         self.assertEqual(self.classObject._host, self.url)
 
-    @mock.patch('requests.sessions.Session.get')
-    def test_get(self, mock_get):
-        url = sm_url + 'bgp:bgp'
-        self.classObject.get('bgp:bgp')
-        mock_get.assert_called_once_with(url, params={})
+    def test_get(self):
+        res = self.classObject.get()
+        self.assertEqual(res.status_code, 200)
 
-    @mock.patch('requests.sessions.Session.put')
-    def test_put_good_data(self, mock_put):
+    def test_put_good_data(self):
         # Tests it with good data
-        with open(os.path.join(here, 'json/put_data.json')) as f:
+        with open(os.path.join(here, '../examples/json/put_data.json')) as f:
             contents = f.read()
-        url = sm_url + endpoint
-        self.classObject.put(contents, endpoint)
-        mock_put.assert_called_once_with(url, data=contents)
+        res = self.classObject.put(contents, endpoint)
+        self.assertEqual(res.status_code, 204)
 
-    # @mock.patch('requests.sessions.Session.put')
-    # def test_put_bad_data(self, mock_put):
-    #     # Test it with bad data
-    #     with open(os.path.join(here, 'json/invalid_data.json')) as f:
-    #         contents = f.read()
-    #     put_res = mock_put(contents, 'bgp:bgp')
-    #     self.assertEqual(put_res.status_code,
-    #                      mock_put.return_value.status_code)
-
-    @mock.patch('requests.sessions.Session.patch')
-    def test_patch_good_data(self, mock_patch):
-        with open(os.path.join(here, 'json/patch_data.json')) as f:
-            contents = f.read()
-        patch_res = mock_patch(
-            contents,
-            'Cisco-IOS-XR-ip-static-cfg:router-static'
-            )
-        mock_patch.assert_called_once_with(
-            contents,
-            'Cisco-IOS-XR-ip-static-cfg:router-static'
-            )
-        self.assertEqual(patch_res.status_code,
-                         mock_patch.return_value.status_code)
-
-    @mock.patch('rest_calls.jsonRestClient.JSONRestCalls.patch')
-    def test_patch_bad_data(self, mock_patch):
-        with open(os.path.join(here, 'json/invalid_data.json')) as f:
-            contents = f.read()
-        patch_res = mock_patch(contents, 'bgp:bgp')
-        self.assertEqual(patch_res.status_code,
-                         mock_patch.return_value.status_code)
-
-    @mock.patch('rest_calls.jsonRestClient.JSONRestCalls.post')
-    def test_post_good_data(self, mock_post):
-        with open(os.path.join(here, 'json/put_data.json')) as f:
-            contents = f.read()
-        post_res = mock_post(
-            contents,
-            'Cisco-IOS-XR-ip-static-cfg:router-static'
-            )
-        mock_post.assert_called_once_with(
-            contents,
-            'Cisco-IOS-XR-ip-static-cfg:router-static'
-            )
-        self.assertEqual(post_res.status_code,
-                         mock_post.return_value.status_code)
-
-    @mock.patch('rest_calls.jsonRestClient.JSONRestCalls.post')
-    def test_post_bad_data(self, mock_post):
+    def test_put_bad_data(self):
         # Test it with bad data
-        with open(os.path.join(here, 'json/invalid_data.json')) as f:
+        with open(os.path.join(here, '../examples/json/invalid_data.json')) as f:
             contents = f.read()
-        post_res = mock_post(contents, 'bgp:bgp')
-        self.assertEqual(post_res.status_code,
-                         mock_post.return_value.status_code)
+        put_res = self.classObject.put(contents, endpoint)
+        self.assertEqual(put_res.status_code, 400)
+
+    def test_patch_good_data(self):
+        with open(os.path.join(here, '../examples/json/patch_data.json')) as f:
+            contents = f.read()
+        res = self.classObject.patch(contents, endpoint)
+        self.assertEqual(res.status_code, 204)
+
+    def test_patch_bad_data(self):
+        with open(os.path.join(here, '../examples/json/invalid_data.json')) as f:
+            contents = f.read()
+        res = self.classObject.patch(contents, endpoint)
+        self.assertEqual(res.status_code, 400)
+
+    def test_post_good_data(self):
+        with open(os.path.join(here, '../examples/json/put_data.json')) as f:
+            contents = f.read()
+        res = self.classObject.post(contents, endpoint)
+        self.assertEqual(res.status_code, 204)
+
+    def test_post_bad_data(self):
+        # Test it with bad data
+        with open(os.path.join(here, '../examples/json/invalid_data.json')) as f:
+            contents = f.read()
+        res = self.classObject.post(contents, endpoint)
+        self.assertEqual(res.status_code, 400)
+
+    def test_delete(self):
+        res = self.classObject.delete(endpoint)
+        self.assertEqual(res.status_code, 204)
 
 
 if __name__ == "__main__":
