22411f3907f55f4d1723d3448d32c4f59c071ba2
kkumara3 <kkumara3@cisco.com>
2016-07-28 23:52:50 +0000

    add EOR checks and checks for ok reply

--- solenoid/tests/integration/test_rib.py
+++ solenoid/tests/integration/test_rib.py
@@ -14,36 +14,37 @@ class RestRibTestCase(unittest.TestCase, object):
         open(tools.add_location('../logs/debug.log'), 'w').close()
         open(tools.add_location('../logs/errors.log'), 'w').close()
 
-    def test_create_transport_object_correct_class_created(self):
-        transport_object = edit_rib.create_transport_object()
-        self.assertIsInstance(transport_object, edit_rib.JSONRestCalls)
-
-    def test_rib_announce_rest(self):
+    def test_rib_1announce_rest(self):
         with open(tools.add_location('examples/integration/rendered_announce.txt')) as f:
             rendered_announce = f.read()
         edit_rib.rib_announce(rendered_announce)
-        self.assertIn('| ANNOUNCE | ', tools.check_debuglog()[0])
+        self.assertIn('| ANNOUNCE | OK', tools.check_debuglog()[0])
 
-    def test_rib_withdraw_rest(self):
+    def test_rib_2withdraw_rest(self):
         withdraw_prefixes = ['1.1.1.1/32',
                      '1.1.1.2/32',
                      '2.2.2.2/32',
                      '3.3.3.3/32']
         edit_rib.rib_withdraw(withdraw_prefixes)
-        self.assertIn('| WITHDRAW | ', tools.check_debuglog()[0])
+        self.assertIn('| WITHDRAW | OK', tools.check_debuglog()[0])
 
-    def test_rib_announce_rest_json(self):
+    def test_rib_3announce_rest_json(self):
         with open(tools.add_location('examples/integration/exa-announce.json')) as f:
             exa_announce = f.read()
         edit_rib.render_config(json.loads(exa_announce))
-        self.assertIn('| ANNOUNCE | ', tools.check_debuglog()[0])
+        self.assertIn('| ANNOUNCE | OK', tools.check_debuglog()[0])
 
-    def test_rib_withdraw_rest_json(self):
+    def test_rib_4withdraw_rest_json(self):
         with open(tools.add_location('examples/integration/exa-withdraw.json')) as f:
             exa_withdraw = f.read()
         edit_rib.render_config(json.loads(exa_withdraw))
-        self.assertIn('| WITHDRAW | ', tools.check_debuglog()[0])
+        self.assertIn('| WITHDRAW | OK', tools.check_debuglog()[0])
 
+    def test_rib_5announce_rest_EOR(self):
+        with open(tools.add_location('examples/exa/exa-eor.json')) as f:
+            exa_announce_eor = f.read()
+        edit_rib.render_config(json.loads(exa_announce_eor))
+        self.assertIn('EOR message\n', tools.check_debuglog()[0])
 if __name__ == '__main__':
     unittest.main(failfast=True)
 
